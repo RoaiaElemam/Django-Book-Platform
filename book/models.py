9c6a78584f5794e3965b15cefe99c7edfa89c32e
RoaiaElemam <mroaia219@gmail.com>
2023-11-13 01:14:47 +0200

    design book models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Author (models.Model):
+    name=models.CharField(max_length=30)
+    birth_date=models.DateField()
+    biography=models.TextField(max_length=5000)
+
+    def __str__(self):
+      return self.name
+
+
+class Book (models.Model):
+    title=models.CharField(max_length=30)
+    publish_date=models.DateTimeField(default=timezone.now)
+    price=models.IntegerField()
+    author=models.ForeignKey('Author',on_delete=models.CASCADE,related_name='book_author')
+
+    def __str__(self):
+      return self.title
+
+
+class Review (models.Model):
+    book=models.ForeignKey('Book',on_delete=models.CASCADE,related_name='review_book')
+    reviewer_name=models.CharField(max_length=30)
+    content=models.TextField(max_length=50000)
+    rating = models.SmallIntegerField(choices=[(i, i) for i in range(1, 5)],default=1)
+
+    
+
+    def __str__(self):
+      return self.book
