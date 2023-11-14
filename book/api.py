f5895adc9d086d63e829363a51d5f99ac822bad8
RoaiaElemam <mroaia219@gmail.com>
2023-11-14 02:13:06 +0200

    add create update delete api

--- book/api.py
+++ book/api.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework import filters
-from rest_framework.response import Response
 
 from .serializer import BookSerializer
 from .models import Book
@@ -10,7 +9,7 @@ from .models import Book
 
 
 
-class BookListApi(generics.ListAPIView):
+class BookListApi(generics.ListCreateAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
   #  filter_backends = [DjangoFilterBackend, filters.SearchFilter,filters.OrderingFilters]
@@ -19,6 +18,6 @@ class BookListApi(generics.ListAPIView):
     #ordering_fields = ['salary_start', 'salary_end','experince']
 
 
-class BookDetailApi(generics.RetrieveAPIView):
+class BookDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
